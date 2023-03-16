b2c77f7c60d48a978f0c42dc04b675c2d75aa2ac
milenium125 <dsanmiguel582@gmail.com>
2023-03-15 20:13:29 -0500

    add crud

--- routes/router.js
+++ routes/router.js
@@ -8,9 +8,13 @@ router.get('/', (req, res)=>{res.send('<h1>Hola Mundo</h1>').status(200)});
 
 //ENDPOINT LOCALS
 router.get('/locales', controllerLocal.listBusinessLocal); // Search all locals
-router.get('/locales/:id'); // Search locals for id
+router.get('/locales/:user', controllerLocal.searchByUserName); // Search locals for id
 
 router.post('/locales', controllerLocal.saveBusinessLocal); // Save a local
+
+router.put('/locales/:user', controllerLocal.updateBusinessLocal);
+
+router.delete('/locales/:user', controllerLocal.deleteBusinessLocal);
 
 
 module.exports = router;
--- src/controllers/businessLocal_controller.js
+++ src/controllers/businessLocal_controller.js
@@ -2,19 +2,31 @@ const businessLocal = require('../models/Local');
 
 //CRUD
 //Create a Business Local with all information
-function saveBusinessLocal(req, res){
-    console.log('guardando');
+async function saveBusinessLocal(req, res){
+    //console.log('guardando');
     var object_local = new businessLocal(req.body);
+    await businessLocal.exists({"_user_local": object_local._user_local}, async function(err, doc){
+        if(err){
+            res.status(404).send({message:"user Existing"});
+            console.log(err);
+        }else{
+            if(!doc){
+                object_local.save((err,result)=>{
+                    res.status(200).send(result);
+                });
+            }else{
+                res.status(404).send({message:"user Existing"});
+            }
+        }
+    })
 
-    console.log(object_local);
+    //console.log(object_local);
 
-    object_local.save((err,result)=>{
-        res.status(200).send(result);
-    });
+    
 }
 
 function listBusinessLocal(req, res){
-    if(!req.params.id){
+    if(!req.params.user){
         var result = businessLocal.find({}).sort("_type_local");
         result.exec(function(err, result){
             if(err){
@@ -37,10 +49,58 @@ function listBusinessLocal(req, res){
     }
 }
 
+async function searchByUserName(req, res){
+    let user_name = req.params.user;
+
+    let local = await businessLocal.find({"_user_local":user_name});
+    //console.log(local);
+    if(local){
+        res.status(200).send({message:local});
+    }else{
+        res.status(404).send({message:"Not Found"});
+    }
+    //console.log(user_name);
+}
+
+async function updateBusinessLocal(req, res){
+    let user_name = req.params.user;
+
+    await businessLocal.exists({"_user_local": user_name}, async function(err, doc){
+        if(err){
+            console.log(err);
+        }else{
+            if(doc){
+                let result = await businessLocal.updateOne({"_user_local": user_name});
+                res.status(200).send({message:result});
+            }else{
+                res.status(404).send({message:"Not Found"});
+            }
+        }
+    })
+
+}
+async function deleteBusinessLocal(req, res){
+    let user_name = req.params.user;
+    await businessLocal.exists({"_user_local": user_name}, async function(err, doc){
+        if(err){
+            console.log(err);
+        }else{
+            if(doc){
+                let result = await businessLocal.deleteOne({"_user_local": user_name});
+                res.status(200).send({message:result});
+            }else{
+                res.status(404).send({message:"Not Found"});
+            }
+        }
+    })
+}
 
 
 
 module.exports = {
     saveBusinessLocal,
-    listBusinessLocal
+    listBusinessLocal,
+    searchByUserName,
+    updateBusinessLocal,
+    deleteBusinessLocal
 };
